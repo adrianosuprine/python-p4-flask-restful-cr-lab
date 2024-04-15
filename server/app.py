c49680107a69c8c7773157845bf96569d9e63dda
adrianosuprine <adrianosuprine@gmail.com>
2024-04-15 17:56:52 +0300

    restful cr

--- server/app.py
+++ server/app.py
@@ -17,11 +17,35 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [plant.to_dict() for plant in Plant.query.all()] 
+        return jsonify(response_dict_list)
+    
+    def post(self):
+        data = request.get_json()
+        if not data or 'name' not in data or 'image' not in data or 'price' not in data:
+            return make_response(jsonify({"error": "Missing required data"}), 400)
 
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return make_response(jsonify(new_plant.to_dict()), 201)
+api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        plant = Plant.query.filter_by(id = id).first()
+        if plant:
+            return jsonify(plant.to_dict())
+        else:
+            return jsonify({'message':'Plant not found'}), 404
         
 
+api.add_resource(PlantByID, '/plants/<int:id>')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
